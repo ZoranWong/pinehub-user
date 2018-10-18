02e12cdad453ed0bb3233b2b27244965befe1c54
徐钰 徐 <xuyu@xuyudeMacBook-Air.local>
2018-10-18 21:11:52 +0800

    改进路由

---
+++ src/models/Categories.js
@@ -0,0 +1,6 @@
+import Model from './Model';
+export default class Categories extends Model {
+	constructor(app) {
+		super(app);
+	}
+}
--- src/pages/my/mystore/status/main.js
+++ src/pages/my/mystore/status/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue';
 import App from './index';
 import Application from '../../../../Application';
 import _ from 'underscore';
+import Merchandises from '@/models/Merchandises';
+import Categories from '@/models/Categories';
 const myMystoreStatus = new Application(App, 'my.mystore.status');
 myMystoreStatus.run((app) => {
+	app.models.addModel('myStore.merchandises', Merchandises);
+	app.models.addModel('store.categories',Categories)
 }, (mountComponent) => {
 	_.extend(App, mountComponent);
 	let app = new Vue(App);
