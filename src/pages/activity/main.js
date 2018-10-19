e728de9390439400d58f5838f2323d65b2af0d84
wong1989 <skyandsunning@163.com>
2018-10-19 23:02:16 +0800

    添加model注册接口

--- src/pages/activity/main.js
+++ src/pages/activity/main.js
@@ -14,24 +14,12 @@ application.run(function(app) {
 		//app.registerModel('model.activity.merchandises', Merchandises);
 		app.models.addModel('model.activity.merchandises', Merchandises);
 		app.models.addModel('model.shoppingCarts', ShoppingCarts);
-	}	
+	}
 	app.registerCommand(GetMerchandisesCommand.commandName(), GetMerchandisesCommand);
-<<<<<<< HEAD
-}, function(mountComponent) {
-	_.extend(App, mountComponent);
-	const app = new Vue(App);
-	app.$mount();
-	return app;
-});
-=======
 	app.registerCommand(AddMerchandiseCommand.commandName(),AddMerchandiseCommand);
 },function(mountComponent) {
- _.extend(App,mountComponent);
- const app = new Vue(App);
- app.$mount();
- return app;
+	 _.extend(App,mountComponent);
+	 const app = new Vue(App);
+	 app.$mount();
+	 return app;
 });
-
-
-
->>>>>>> fc9bce91f6936a5928c5f25ca521de5cf1dbe499
