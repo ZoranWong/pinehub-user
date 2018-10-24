3d61b41ec116c319eb2fc029b96fad1d355271c0
zhihong01 <1044432205@qq.com>
2018-10-24 21:29:56 +0800

    添加配送订单

--- src/pages/reserveShop/index.vue
+++ src/pages/reserveShop/index.vue
@@ -62,7 +62,7 @@
         this.isShow = false;
       },
       next() {
-        this.$command('GET_MERCHANDISE_LIST', 'model.activity.merchandises/setList', 'activity', this.activityId, this.currentPage + 1, this.pageCount);               
+      this.$command('GET_MERCHANDISE_LIST', 'model.activity.merchandises/setList', 'activity', this.activityId, this.currentPage + 1, this.pageCount);               
       },
       addCart(shopId, count,  merchandiseId){
         this.$command('ADD_MERCHANDISE_TO_CART', merchandiseId, count, shopId);
