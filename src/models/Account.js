7e7196e3cf42cd5be19f2254db103998484ea7d1
ZoranWong <904110231@qq.com>
2018-11-14 14:03:27 +0800

    优化小程序application 与命令

--- src/models/Account.js
+++ src/models/Account.js
@@ -1,42 +1,41 @@
 import Model from './Model';
 import _ from 'underscore';
 export default class Account extends Model {
-	constructor(app) {
-		super(app);
-	}
-	computed() {
-		return _.extend(super.computed(), {
-			isAuth(state) {
-				return state.openId !== null;
-			},
-			isMember(state) {
-				return state.mobile !== null;
-			},
-			userScore(state) {
-				return state.userScore;
-			},
-			token(state) {
-				return state.token;
-			}
-		});
-	}
-	data() {
-//		let userInfo = this.services();
-		return {
-			openId: null,
-			unionId: null,
-			avator: null,
-			mobile: null,
-			userScore: null,
-			token: null
-		};
-	}
-	//监听数据
-	listeners() {
-		this.addEventListener('setAccount', function(userInfo) {
-			_.extend(this.state, userInfo);
-//			this.services('mp.storage').set('token', userInfo.token);
-//			this.services('mp.storage').set('userInfo', userInfo);
-		});
-	}
+    constructor (app) {
+        super(app);
+    }
+    computed () {
+      return _.extend(super.computed(), {
+          isAuth (state) {
+              return state.openId !== null;
+          },
+          isMember (state) {
+              return state.mobile !== null;
+          },
+          userScore (state) {
+              return state.userScore;
+          },
+          token (state) {
+              return state.token;
+          }
+      });
+    }
+    data () {
+        return {
+            openId: null,
+            unionId: null,
+            avatar: null,
+            mobile: null,
+            userScore: null,
+            token: null
+        };
+    }
+    // 监听数据
+    listeners () {
+        this.addEventListener('setAccount', function(userInfo) {
+            _.extend(this.state, userInfo);
+            this.services('mp.storage').set('token', userInfo.token);
+            this.services('mp.storage').set('userInfo', userInfo);
+        });
+    }
 }
