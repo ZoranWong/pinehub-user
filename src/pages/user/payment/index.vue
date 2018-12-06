07d44030c955e7acb4fe89c7eb54ac669c00b2c5
ZoranWong <904110231@qq.com>
2018-12-06 19:41:46 +0800

    添加微信买单页面

---
+++ src/pages/user/payment/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="body">
+        <div class = "payment-box">
+
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data: {
+
+        },
+        created () {
+
+        },
+        mounted () {
+
+        }
+    }
+</script>
