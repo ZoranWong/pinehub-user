1ac6c76205cf4889b158b8a349313160a9aae5fc
ZoranWong <904110231@qq.com>
2020-02-25 16:29:48 +0800

    添加notification通知

--- src/services/websocket/WebSocketService.js
+++ src/services/websocket/WebSocketService.js
@@ -1,30 +1,41 @@
 import Service from "../Service";
 import Echo from 'laravel-echo'
+import th from "element-ui/src/locale/lang/th";
+
 const io = require('weapp.socket.io')
 
 export default class WebSocketService extends Service {
-    constructor (application) {
+    _gateway = null;
+    _connection = null;
+
+    constructor(application) {
         super(application);
         console.log(application);
-        this.gateway = this.$application.config['app']['socket']['gateway'];
-        this.connection = new Echo({
-            client: io,
-            broadcaster: 'socket.io',
-            host: this.gateway,
-            disableStats: true,
-            enabledTransports: ['ws', 'wss']
-        });
+        this._gateway = this.$application.config['app']['socket']['gateway'];
     }
 
-    onOpen () {
-
+    get connection() {
+        return (async () => {
+            let token = await this.service('mp.auth').getToken();
+            this._connection = this._connection ? this._connection : new Echo({
+                client: io,
+                broadcaster: 'socket.io',
+                host: this._gateway,
+                disableStats: true,
+                enabledTransports: ['ws', 'wss'],
+                auth: {
+                    headers: {
+                        'Authorization': 'Bearer ' + token // getToken前端自行实现
+                    }
+                }
+            });
+            console.log(token, this._connection);
+            return this._connection;
+        })();
     }
 
-    onMessage () {
-
-    }
-
-    onClose () {
-
+    async notification(userId, callback) {
+        let connection = await this.connection;
+        connection.private(`App.Models.User.${userId}`).notification(callback);
     }
 }
