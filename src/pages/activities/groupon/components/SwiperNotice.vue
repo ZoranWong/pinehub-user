add8d6c2d13e40b07da4ba47fcbf30096742dbf3
Q <42404280+MeasponeS@users.noreply.github.com>
2020-04-24 14:51:29 +0800

    拼团详情

---
+++ src/pages/activities/groupon/components/SwiperNotice.vue
@@ -0,0 +1,86 @@
+<!--suppress ALL -->
+<template>
+    <div class="swiperNotices">
+        <swiper class="swiper" circular="true" :indicator-dots="false" autoplay="true" interval="500000" duration="1000" >
+            <block v-for="(item, index) in notices" :index="index" :key="index">
+                <swiper-item class="swiperNoticeItem">
+                    <image :src="item.img" class="slide-image" mode="aspectFill"/>
+                    <h4>{{item.title}}</h4>
+                </swiper-item>
+            </block>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SwiperNotice",
+        data () {
+            return {
+                notices: [
+                    {
+                        img: 'https://kingdomcloud.oss-cn-hangzhou.aliyuncs.com/2020/02/08/生日蛋糕.png',
+                        title: '张强刚刚下单购买了面包！'
+                    },
+                    {
+                        img: 'https://kingdomcloud.oss-cn-hangzhou.aliyuncs.com/2020/02/08/生日蛋糕.png',
+                        title: '王学红刚刚下单购买了面包！'
+                    },
+                    {
+                        img: 'https://kingdomcloud.oss-cn-hangzhou.aliyuncs.com/2020/02/08/生日蛋糕.png',
+                        title: '曹文君刚刚下单购买了面包！'
+                    },
+                    {
+                        img: 'https://kingdomcloud.oss-cn-hangzhou.aliyuncs.com/2020/02/08/生日蛋糕.png',
+                        title: '叶长旺刚刚下单购买了面包！'
+                    },
+                    {
+                        img: 'https://kingdomcloud.oss-cn-hangzhou.aliyuncs.com/2020/02/08/生日蛋糕.png',
+                        title: '余晓东刚刚下单购买了面包！'
+                    },
+                    {
+                        img: 'https://kingdomcloud.oss-cn-hangzhou.aliyuncs.com/2020/02/08/生日蛋糕.png',
+                        title: '高罗刚刚下单购买了面包！'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+    .swiperNotices{
+        max-width: 500rpx;
+        min-width: 200rpx;
+        height: 50rpx;
+        background:linear-gradient(0deg,rgba(252,210,100,1),rgba(252,236,125,1));
+        border-radius:25rpx;
+    }
+
+    .swiperNotices .swiper{
+        height: 50rpx!important;
+    }
+
+    .swiperNoticeItem{
+        width:fit-content;
+        width:-webkit-fit-content;
+        width:-moz-fit-content;
+        /*display:inline-block;*/
+        padding: 6rpx;
+        box-sizing: border-box;
+    }
+
+    .swiperNoticeItem image{
+        width: 38rpx;
+        height: 38rpx;
+        border-radius: 50%;
+    }
+
+    .swiperNoticeItem h4{
+        display: inline;
+        font-size: 22rpx;
+        color: #111;
+        margin-left: 11rpx;
+        font-weight: normal;
+    }
+</style>
--- src/pages/activities/groupon/grouponDetails/index.vue
+++ src/pages/activities/groupon/grouponDetails/index.vue
@@ -1,14 +1,19 @@
 <!--suppress ALL -->
 <template>
 	<div id="groupon_details">
-        拼团详情页
+        <div class="backImg">
+            <img src="./img/background.jpeg" mode="widthFix" alt="">
+        </div>
+        <img src="./img/background.jpeg" alt="" class="image">
+        <SwiperNotice />
 	</div>
 </template>
 <script>
-    import _ from 'underscore'
+    import _ from 'underscore';
+    import SwiperNotice from "../components/SwiperNotice";
 	export default {
 		components: {
-
+            SwiperNotice
 		},
 		data() {
 			return {
@@ -39,7 +44,31 @@
         overflow: hidden;
 	}
 
+    /*.swiperNotices{*/
+    /*    width: auto;*/
+    /*    height: 100rpx;*/
+    /*}*/
 
+    .backImg{
+        width: 100%;
+        height: 200rpx;
+        overflow: hidden;
+    }
+
+    .backImg img{
+        width: 100%;
+        height: auto;
+        -webkit-filter: blur(40px);
+        -moz-filter: blur(40px);
+        -ms-filter: blur(40px);
+        filter: blur(40px);
+        transform: translateY(-226rpx);
+    }
+
+    .groupon_details .image{
+        width: 228rpx;
+        height: 221rpx;
+    }
 
 
 </style>
