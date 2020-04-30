8bc1702780d9d910f03ee8854ba3ffae45a83ffd
Q <42404280+MeasponeS@users.noreply.github.com>
2020-04-30 22:43:38 +0800

    Update index.vue

--- src/pages/activities/activities/index.vue
+++ src/pages/activities/activities/index.vue
@@ -1,35 +1,83 @@
 <!--suppress ALL -->
 <template>
-    <div v-if="showView">
-        <web-view
-            :src="url"
-            @message="receive"
-        ></web-view>
+    <div>
+        <div v-if="showView">
+            <web-view
+                :src="url"
+                @message="receive"
+            ></web-view>
+        </div>
+        <Auth v-if="getAuth" @close="closeAuth" />
+        <GetUserMobile v-if="showBindMobile" @close="closeGetUserMobile" />
     </div>
+
 </template>
 
 <script>
     import _ from 'underscore';
+    import Auth from "../../../components/Auth";
+    import GetUserMobile from "../../../components/GetUserMobile";
     export default {
-		name: 'index',
+        name: 'index',
         data(){
-		    return {
+            return {
                 url: '',
                 showView: false,
-                query: {},
-                token: ''
+                options: {},
+                getAuth: false,
+                showBindMobile: false
             }
+        },
+        components: {
+            Auth
         },
         destroyed () {
 
         },
         watch: {
-		    url (val) {
-		        if (val) {
+            url (val) {
+                if (val) {
                     console.log(val, '&&&&&&');
                     this.showView = true
                 }
-		    }
+            },
+            token (val) {
+                console.log(val, '(((((((((((((((((((((((((((((((((((((');
+                if (val) {
+                    wx.setNavigationBarTitle({
+                        title: decodeURI(this.options.name)
+                    });
+                    wx.setNavigationBarColor({
+                        frontColor: '#000000',
+                        backgroundColor: '#f2f2f2',
+                        animation: {
+                            duration: 400,
+                            timingFunc: 'easeIn'
+                        }
+                    })
+                    let gateway = this.config['app']['http']['gateway'];
+                    let env = this.config['app']['env'];
+                    let str = Math.random().toString(36).substr(2)
+                    this.url = `https://neptune.klsfood.cn/activity.html?activity_id=${this.options.id}&token=${val}&headHeight=${this.headHeight}&mainHeight=${this.mainHeight}&random=${str}&env=${env}`;
+                    console.log(this.url, 'activity uel');
+                    // this.url = `http://activity.node:8091/?activity_id=6`;
+                } else {
+                    this.getUserAuth()
+                }
+            },
+            registered (value) {
+                if (value) {
+                    this.closeAuth()
+                }
+                if (value && !this.isMember) {
+                    this.showBindMobile = true
+                }
+            },
+            isMember (val) {
+                if (val) {
+                    this.showBindMobile = false;
+                }
+            },
         },
         computed :{
             statusBarHeight () {
@@ -46,39 +94,84 @@
                 let height = systemInfo.windowHeight;
                 return height - this.headHeight;
             },
-        },
-        onShareAppMessage: function (res) {
-            console.log(this.query, '==========>');
-            //可以先看看页面数据都有什么，得到你想要的数据
-            return {
-                title: this.query.data.name,
-                desc: "青松易购小程序",
-                imageUrl: "",
-                path: `/pages/activities/activities/main?url=${this.query.data.url}`,
-                success: function (res) {
-                    // 转发成功
-                    console.log("转发成功:" + JSON.stringify(res));
-                },
-                fail: function (res) {
-                    // 转发失败
-                    console.log("转发失败:" + JSON.stringify(res));
+            token(){
+                if (this.model.account.token) {
+                    return this.model.account.token.value;
+                } else {
+                    return null
                 }
-            }
+
+            },
+            registered () {
+                return this.model.account.registered;
+            },
+            isAuth () {
+                return this.model.account.isAuth;
+            },
+            isMember () {
+                return this.model.account.isMember;
+            },
         },
+        // onShareAppMessage: function (res) {
+        //     console.log(this.options, '==========>');
+        //     //可以先看看页面数据都有什么，得到你想要的数据
+        //     return {
+        //         title: decodeURI(this.options.name),
+        //         desc: "青松易购小程序",
+        //         imageUrl: "",
+        //         path: `/pages/activities/activities/main?id=${this.options.id}&name=${this.options.name}`,
+        //         success: function (res) {
+        //             // 转发成功
+        //             console.log("转发成功:" + JSON.stringify(res));
+        //         },
+        //         fail: function (res) {
+        //             // 转发失败
+        //             console.log("转发失败:" + JSON.stringify(res));
+        //         }
+        //     }
+        // },
         methods: {
             receive (data) {
                 console.log(data, 'receive');
+            },
+            getUserAuth () {
+                this.getAuth = true
+            },
+            closeAuth () {
+                this.getAuth = false
+            },
+            closeGetUserMobile () {
+                this.showBindMobile = false
             }
+
         },
         onShow () {
-            console.log('我又进来啦');
-            if (this.query && this.query.data) {
+            let pages =  getCurrentPages();
+            console.log(pages, 'xxxxxxx');
+            let options = pages[pages.length - 1]['options'];
+            this.options = options;
+            console.log(options, '========>>>>>>>>>');
+            if (this.token) {
+                wx.setNavigationBarTitle({
+                    title: decodeURI(this.options.name)
+                });
+                wx.setNavigationBarColor({
+                    frontColor: '#000000',
+                    backgroundColor: '#f2f2f2',
+                    animation: {
+                        duration: 400,
+                        timingFunc: 'easeIn'
+                    }
+                })
                 let gateway = this.config['app']['http']['gateway'];
                 let env = this.config['app']['env'];
                 let str = Math.random().toString(36).substr(2)
                 console.log(str, '===>');
-                this.url = `https://neptune.klsfood.cn/activity.html${this.query.data.url}&token=${this.token}&headHeight=${this.headHeight}&mainHeight=${this.mainHeight}&random=${str}&env=${env}`;
+                this.url = `https://neptune.klsfood.cn/activity.html?activity_id=${options.id}&token=${this.token}&headHeight=${this.headHeight}&mainHeight=${this.mainHeight}&random=${str}&env=${env}`;
+            } else {
+                this.getUserAuth()
             }
+
         },
         onHide () {
             console.log('on hide');
@@ -90,30 +183,9 @@
         },
         components: {},
         mounted () {
-            let query = this.$route.query;
-            this.query = this.$route.query;
-            console.log(this);
-            let token = this.model.account.token.value;
-            this.token = token;
-            wx.setNavigationBarTitle({
-                title: query.data.name
-            });
-            wx.setNavigationBarColor({
-                frontColor: '#000000',
-                backgroundColor: '#f2f2f2',
-                animation: {
-                    duration: 400,
-                    timingFunc: 'easeIn'
-                }
-            })
-            let options = pages[pages.length - 1]['options'];
-            let gateway = this.config['app']['http']['gateway'];
-            let env = this.config['app']['env'];
-            let str = Math.random().toString(36).substr(2);
-            let url = options.url? options.url : query.data.url
-            this.url = `https://neptune.klsfood.cn/activity.html${query.data.url}&token=${token}&headHeight=${this.headHeight}&mainHeight=${this.mainHeight}&random=${str}&env=${env}`;
-            console.log(this.url, 'activity uel');
-            // this.url = `http://activity.node:8091/?activity_id=6`;
+            if (!this.token) {
+                this.getUserAuth()
+            }
         }
     }
 </script>
